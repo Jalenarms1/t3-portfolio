f51b7ab0ecfa154c28e1e677563e793431ac819c
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-12-22 07:53:22 -0700

    adj home page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,9 +31,9 @@ const Home: NextPage = () => {
           
           </Link>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-slate-400">Email</p>
-          <p className="text-slate-400">jalenarms@outlook.com</p>
+        <div className="flex flex-col items-center gap-2 mt-5">
+          <a href="mailto:jalenarms@outlook.com" className="text-slate-400 hover:text-cyan-600">Email </a>
+          <a href="mailto:jalenarms@outlook.com" className="text-slate-400 hover:text-cyan-600">jalenarms@outlook.com</a>
         </div>
         <div className="flex flex-col items-center gap-2">
           <p className="text-slate-400">Phone</p>
