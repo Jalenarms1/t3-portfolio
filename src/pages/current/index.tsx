a103a0bbfcddf2efecdd3c4094aa8dc46b9976fd
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-12-23 14:22:26 -0700

    added currently page

--- src/pages/current/index.tsx
+++ src/pages/current/index.tsx
@@ -11,7 +11,7 @@ export default function Current() {
                 <>
                     <div className='flex flex-col items-center gap-8'>
                         <ProjectCard proj={proj} />
-                        <div className='flex flex-col md:w-[70%] lg:w-[60%] sm:w-[70%] xl:w-[50%] 2xl:w-[45%]  m-auto p-4 rounded shadow-md shadow-cyan-600'>
+                        <div className='flex flex-col sm:[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[45%]  m-auto p-4 rounded shadow-md shadow-cyan-600'>
 
                             <p className='text-slate-50 leading-loose'> 
                                 I have been freelancing for a start-up web application with an employer that hired me through <a href="https://www.upwork.com/freelancers/~01cd6b779e11f90eb8" rel='noreferrer' target="_blank" className='underline text-cyan-500 hover:text-cyan-600'>upwork</a>. I have restructured the database to simplify the tables where there was reocurring data and created a new table as a central source of reference for the original tables to have a relation to.
