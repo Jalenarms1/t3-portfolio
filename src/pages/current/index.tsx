118895f9f07c6ea53d707b2fa643e5bb4302c039
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-12-23 22:42:54 -0700

    adding more to current description

--- src/pages/current/index.tsx
+++ src/pages/current/index.tsx
@@ -17,7 +17,7 @@ export default function Current() {
                                 I have been freelancing for a start-up web application with an employer that hired me through <a href="https://www.upwork.com/freelancers/~01cd6b779e11f90eb8" rel='noreferrer' target="_blank" className='underline text-cyan-500 hover:text-cyan-600'>upwork</a>. I have restructured the database to simplify the tables where there was reocurring data and created a new table as a central source of reference for the original tables to have a relation to.
                                 <br />
                                 <br />
-                                On the frontend, I have added a directory page to allow the user to search and filter through all of the data that exists, and find exactly what they are looking for.
+                                I have also added a directory page and was soley responsible for all of its functionality. It required desiging a function in the server to communicate with the database to query for certain pieces of information based on the requested subject or filters, if any. A cool feature I added on was pagination, so the user can click a button to populate more information. This eliminates the need to query too much data at once, when the user can just click to see more as much as they want.
 
                             </p>
 
