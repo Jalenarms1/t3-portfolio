81760fd93a532eeb465978cd6a9a32c287f793ed
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-12-25 21:10:05 -0700

    changed home icon

--- src/pages/current/index.tsx
+++ src/pages/current/index.tsx
@@ -17,7 +17,7 @@ export default function Current() {
                                 I have been freelancing for a start-up web application with an employer that hired me through <a href="https://www.upwork.com/freelancers/~01cd6b779e11f90eb8" rel='noreferrer' target="_blank" className='underline text-cyan-500 hover:text-cyan-600'>upwork</a>. I have restructured the database to simplify the tables where there was reocurring data and created a new table as a central source of reference for the original tables to have a relation to.
                                 <br />
                                 <br />
-                                I have also added a directory page. It required desiging a function in the server to communicate with the database to query for certain pieces of information based on the requested subject or filters, if any. A cool feature I added on was pagination, so the user can click a button to populate more information. This eliminates the need to query too much data at once, when the user can click to see more as much as they want.
+                                I have also added a directory page. It required desiging a function in the server to communicate with the database to query for certain pieces of information based on the requested subject or filters, if any. I used react state to manage filter values that would be used as arugments to the function available to the frontend to query the database with. This project has helped me greatly with querying massive amounts of data and how to do it most efficiently for a smooth user experience.
 
                             </p>
 
